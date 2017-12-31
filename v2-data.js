24b55fa3cdcccd2f8907582167f2307f59aa719e
Alessio Dragoni <groundrace@me.com>
2017-12-31 17:23:13 +0100

    initial translation to italian

--- v2-data.js
+++ v2-data.js
@@ -1,8 +1,8 @@
 let catData={"categories":[
-    {"category":"Deviation", "colour":"#E6B00F",
+{"category":"Deviazione", "colour":"#b78c0b",
     "description":"Emphasise variations (+/-) from a fixed reference point. Typically the reference point is zero but it can also be a target or a long-term average. Can also be used to show sentiment (positive/neutral/negative)",
     "example":"Trade surplus/deficit, climate change"},
-    {"category":"Correlation", "colour":"#977348",
+{"category":"Correlazione", "colour":"#977348",
     "description":"Show the relationship between two or more variables. Be mindful that, unless you tell them otherwise, many readers will assume the relationships you show them to be causal (i.e. one causes the other)",
     "example":"Inflation & unemployment, income & life expectancy"},
 {"category":"Change v Time", "colour":"#28903B",
@@ -18,13 +18,13 @@ let catData={"categories":[
     "description":"Show how a single entity can be broken down into its component elements. If the reader’s interest is solely in the size of the components, consider a magnitude-type chart instead",
     "example":"Fiscal budgets, company structures, national election results"},
 {"category":"Magnitude", "colour":"#A3488C",
-"description":"Show size comparisons. These can be relative (just being able to see larger/bigger) or absolute (need to see fine differences). Usually these show a ‘counted’ number (for example, barrels, dollars or people) rather than a calculated rate or per cent",
-"example":"Commodity production, market capitalisation"},
+    "description":"Show size comparisons. These can be relative (just being able to see larger/bigger) or absolute (need to see fine differences). Usually these show a ‘counted’ number (for example, barrels, dollars or people) rather than a calculated rate or per cent",
+    "example":"Commodity production, market capitalisation"},
 {"category":"Spatial", "colour":"#9C9D94",
-"description":"Used only when precise locations or geographical patterns in data are more important to the reader than anything else. ",
-"example":"Locator maps, population density, natural resource locations, natural disaster risk/impact, catchment areas, variation in election results"},
+    "description":"Used only when precise locations or geographical patterns in data are more important to the reader than anything else. ",
+    "example":"Locator maps, population density, natural resource locations, natural disaster risk/impact, catchment areas, variation in election results"},
 {"category":"Flow", "colour":"#ABBD67",
-"description":"Show the reader volumes or intensity of movement between two or more states or conditions. These might be logical sequences or geographical locations",
-"example":"Movement of funds, trade, migrants, lawsuits, information; relationship graphs"}]
+    "description":"Show the reader volumes or intensity of movement between two or more states or conditions. These might be logical sequences or geographical locations",
+    "example":"Movement of funds, trade, migrants, lawsuits, information; relationship graphs"}]
 }
 
